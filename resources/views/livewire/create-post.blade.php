8aa82b4d7e46c6b8ff7499d3b488716b674cc775
raflykunn <rflyrmdhn03@gmail.com>
2025-06-19 18:43:34 +0800

    [feat] add alpine and some feature about alpine

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -1,6 +1,17 @@
 <div>
     <h2>New Post</h2>
 
+    <div x-data="{ count: 0 }">
+        <span x-text="count"></span>
+
+        <button x-on:click="count++">+</button>
+    </div>
+
+    Current Title: <span x-text="$wire.title.toUpperCase()"></span>
+
+    <button x-on:click="$wire.title = ''">clear title</button>
+    <button type="button" x-on:click="$wire.save()">submit form</button>
+
     <form wire:submit="save">
 
         <label>
@@ -15,6 +26,11 @@
         <label>
             <span>Content</span>
             <input wire:model='content' type="text"><br>
+
+            <small>Words:
+                <span x-text="$wire.content.split(' ').length - 1"></span>
+            </small>
+
             @error('content')
             <em>{{ $message }}</em>
             @enderror
